9e6152f60728e1fd3bf32195525e931318c327c7
Baidi Wang <baidi.wang@journalism.cuny.edu>
2020-11-17 23:53:11 -0500

    sroclling reset

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(() => {
-	$("p[data-id]").each(function () {
+	$("[data-id]").each(function () {
 		const $this_paragraph = $(this);
 		const id = $this_paragraph.data("id");
 		const $img = $(`img[id='${id}']`);
