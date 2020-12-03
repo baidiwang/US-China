382e69069ded21688693a211511e9251c2a09a0f
Baidi Wang <baidi.wang@journalism.cuny.edu>
2020-12-03 11:12:29 -0500

    half draft

--- main.js
+++ main.js
@@ -10,3 +10,55 @@ $(() => {
 		});
 	});
 });
+
+
+
+
+
+const players = {};
+
+function onYouTubeIframeAPIReady() {
+	$(".yt_player").each(function () {
+		const player = $(this);
+		players[player.data("id")] = new YT.Player(player[0], {
+			height: '390',
+			width: '640',
+			videoId: player.data("id"),
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+	});
+
+}
+
+function onPlayerReady(event) {
+	console.log(event);
+	console.log("The player is ready");
+}
+
+function onPlayerStateChange(event) {
+	console.log("The player has changed");
+}
+
+$("body").on("click", ".fa-play", function (e) {
+	e.preventDefault();
+
+	const this_play_button = $(this);
+	const this_controls = this_play_button.closest(".controls");
+	const player_id = this_controls.data("id");
+
+	players[player_id].playVideo();
+});
+
+
+$("body").on("click", ".fa-pause", function (e) {
+	e.preventDefault();
+
+	const this_pause_button = $(this);
+	const this_controls = this_pause_button.closest(".controls");
+	const player_id = this_controls.data("id");
+
+	players[player_id].pauseVideo();
+});
